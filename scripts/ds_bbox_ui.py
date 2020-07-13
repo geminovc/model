ada59017566bdc376c877db11e9b20dedc030c0f
prafull <prafull@mit.edu>
2020-07-13 18:28:15 -0400

    Adding optional adjustment to the drawn box.

--- scripts/ds_bbox_ui.py
+++ scripts/ds_bbox_ui.py
@@ -46,28 +46,72 @@ def line_select_callback(eclick, erelease):
     current = [x1, y1, x2, y2]
     ax.add_patch(rect)
 
+def press(event):
+    global i
+    global img
+    global current
+    global prev_rect
+    global ax
+    if event.key == 'z':
+        if current != None:
+            plt.close()
+
+    if event.key == 'a':
+        if current == None:
+            print("Please create an annotation before adjusting it.")
+        prev_rect.remove()
+        x1, y1, x2, y2 = current
+        center_x = np.abs(x1+x2)/2
+        center_y = np.abs(y1+y2)/2
+        length = min(img.shape[0], img.shape[1])
+        start_x = int(center_x - length/2)
+        start_y = int(center_y - length/2)
+        rect = plt.Rectangle((start_x, start_y), int(length), int(length), fill=False, edgecolor='blue')
+        prev_rect = rect
+        current = [max(0, start_x), max(0, start_y), min(img.shape[0], start_x + length), min(img.shape[1],start_y + length)]
+        ax.add_patch(rect)
+        plt.draw()
+        plt.show()
+
+prev_rect = None
+current = []
+prev_dim = None
+
 for i in range(len(image_names)):
     if len(annotations[image_names[i]]) != 0:
         print(image_names[i], "Done!")
+        current = annotations[image_names[i]]
         continue
-    
+
     img = np.flip(cv2.imread(os.path.join(data_dir, image_names[i])), 2)
+
     xdata = np.linspace(0,9*np.pi, num=301)
     ydata = np.sin(xdata)
 
     fig, ax = plt.subplots()
-    line = ax.imshow(img)
-
-    prev_rect = None
-    current = None
-
+    fig.canvas.mpl_connect('key_press_event', press)
+    line = ax.imshow(img)        
+    
+    if i != 0 and img.shape != prev_dim:
+        if prev_rect != None:
+            prev_rect.remove()
+        
+        prev_rect = None
+        current = []
+    
+    print(current)
+    if current != None and len(current) != 0:
+        x1, y1, x2, y2 = current
+        cur_rect = plt.Rectangle( (min(x1,x2),min(y1,y2)), np.abs(x1-x2), np.abs(y1-y2), fill=False, edgecolor='red')
+        ax.add_patch(cur_rect)
+        prev_rect = cur_rect
 
     rs = RectangleSelector(ax, line_select_callback,
                         drawtype='box', useblit=False, button=[1], 
-                        minspanx=5, minspany=5, spancoords='pixels', 
+                        minspanx=5, minspany=5, spancoords='pixels',
                         interactive=True)
     plt.show()
-
+    prev_dim = img.shape
     print(image_names[i], current)
     annotations[image_names[i]] = current
     with open(video_name + ".pkl", "wb") as handle:
