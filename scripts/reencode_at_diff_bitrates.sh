7338868fdabf2348f6fda53499093e3f04a87599
vibhaa <vibhaa@mit.edu>
2020-07-19 13:57:14 -0400

    copying audio when re-encoding

--- scripts/reencode_at_diff_bitrates.sh
+++ scripts/reencode_at_diff_bitrates.sh
@@ -2,18 +2,18 @@
 # for the speech2gesture dataset
 
 DIR="/data/vibhaa/speech2gesture-master/"
-for speaker in "ellen" "oliver" "conan" #"almaram" "angelica" "rock" "chemistry" "shelly"
+for speaker in "conan" "chemistry" "ellen" "oliver" "seth" "almaram" "angelica" "rock" "shelly"
 do
     echo "SPEAKER: ${speaker}"
     mkdir -p ${DIR}dataset/${speaker}/downsampled
-    for file in ${DIR}dataset/${speaker}/cropped/*.mp4
+    for file in ${DIR}dataset/${speaker}/spatially_cropped/*.mp4
     do
 	fbname=$(basename "$file" .mp4)
 	for i in 50 100 200 500 1000
 	do
            ffmpeg -y -re -i "$file" \
             -c:v libx264 -b:v ${i}k  -preset ultrafast \
-            -an "${DIR}dataset/${speaker}/downsampled/${fbname}_${i}Kb.mp4"
+            -c:a copy "${DIR}dataset/${speaker}/downsampled/${fbname}_${i}Kb.mp4"
 	done
     done
 done
