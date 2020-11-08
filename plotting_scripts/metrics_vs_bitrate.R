9c71c890d89b57a3cbd69d04e2759d6e8e76cdfe
vibhaa <vibhaa@mit.edu>
2020-11-08 15:05:43 -0500

    removing legend, making annotation for spade

--- plotting_scripts/metrics_vs_bitrate.R
+++ plotting_scripts/metrics_vs_bitrate.R
@@ -16,12 +16,15 @@ plot_filename <- args[2]
 data<-read.csv(file)
 
 
-psnr_plot <- ggplot(data[data$bitrate != 900, ], aes(x=bitrate,y=psnr,linetype="Codec H.264",color="Codec H.264")) + 
-        geom_line(size=0.8) +
+psnr_plot <- ggplot(data[data$bitrate != 900, ], aes(x=bitrate,y=psnr,color="Codec H.264")) + 
+        geom_line(size=0.8, linetype="dashed") +
         geom_point(size=2) +
 
-        geom_hline(aes(yintercept=29.241, linetype="Generic Model", color="Generic Model")) +
-        geom_hline(aes(yintercept=30.115, linetype="Personalized Model", color="Personalized Model")) +
+        geom_point(size=2, x=50, y=29.241, aes(color="Generic Model")) +
+        annotate("text", x=95, y=28.241, label="Generic\nModel",  color="#00ba38") +
+        
+        geom_point(size=2, x=50, y=30.115, aes(color="Personalized Model")) +
+        annotate("text", x=150, y=31.115, label="Personalized\nModel",  color="#619cff") +
 
         labs(x="Bitrate (Kb/s)", y="PSNR (dB)", color="approaches", linetype="approaches") +
         
@@ -34,12 +37,16 @@ psnr_plot <- ggplot(data[data$bitrate != 900, ], aes(x=bitrate,y=psnr,linetype="
 ggsave(paste(plot_filename, "_psnr.pdf", sep=""), width=5.7,height=5)
 
 
-ssim_plot <- ggplot(data, aes(x=bitrate,y=ssim,linetype="Codec H.264",color="Codec H.264")) + 
-        geom_line(size=0.8) +
+ssim_plot <- ggplot(data, aes(x=bitrate,y=ssim,color="Codec H.264")) + 
+        geom_line(size=0.8,  linetype="dashed") +
         geom_point(size=2) +
         
-        geom_hline(aes(yintercept=0.918, linetype="Generic Model", color="Generic Model")) +
-        geom_hline(aes(yintercept=0.923, linetype="Personalized Model", color="Personalized Model")) +
+        geom_point(size=2, x=50, y=0.918, aes(color="Generic Model")) +
+        annotate("text", x=95, y=0.903, label="Generic\nModel",  color="#00ba38") +
+        
+        geom_point(size=2, x=50, y=0.923, aes(color="Personalized Model")) +
+        annotate("text", x=150, y=0.933, label="Personalized\nModel",  color="#619cff") +
+        
 
         labs(x="Bitrate (Kb/s)", y="SSIM", color="approaches", linetype="approaches") +
         
@@ -51,12 +58,15 @@ ssim_plot <- ggplot(data, aes(x=bitrate,y=ssim,linetype="Codec H.264",color="Cod
 
 ggsave(paste(plot_filename, "_ssim.pdf", sep=""), width=5.7,height=5)
 
-lpips_plot <- ggplot(data, aes(x=bitrate,y=lpips,linetype="Codec H.264",color="Codec H.264")) + 
-        geom_line(size=0.8) +
+lpips_plot <- ggplot(data, aes(x=bitrate,y=lpips,color="Codec H.264")) + 
+        geom_line(size=0.8, linetype="dashed") +
         geom_point(size=2) +
 
-        geom_hline(aes(yintercept=0.103, linetype="Generic Model", color="Generic Model")) +
-        geom_hline(aes(yintercept=0.058, linetype="Personalized Model", color="Personalized Model")) +
+        geom_point(size=2, x=50, y=0.103, aes(color="Generic Model")) +
+        annotate("text", x=95, y=0.123, label="Generic\nModel",  color="#00ba38") +
+        
+        geom_point(size=2, x=50, y=0.058, aes(color="Personalized Model")) +
+        annotate("text", x=150, y=0.038, label="Personalized\nModel",  color="#619cff") +
         
         labs(x="Bitrate (Kb/s)", y="LPIPS", color="approaches", linetype="approaches") +
         
@@ -79,6 +89,6 @@ prow <- plot_grid(psnr_plot + theme(legend.position="none"),
 # this tells it what order to put it in
 # so basically tells it put legend first then plots with th legend height 20% of the
 # plot
-p <- plot_grid(legend, prow, rel_heights=c(.2,1), ncol =1)
+#p <- plot_grid(legend, prow, rel_heights=c(.2,1), ncol =1)
 
-ggsave(paste(plot_filename,"_metrics.pdf", sep=""), width=12.2, height=5)
+ggsave(paste(plot_filename,"_metrics.pdf", sep=""), width=12.2, height=3.5)
