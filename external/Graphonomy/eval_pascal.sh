a743846e33ecb06ef4f1d4d70376d63f79b349b1
pkarimib <pkarimib@mit.edu>
2021-04-13 12:51:52 -0400

    After the train on minidataset, before assing my metrics to runner file

--- external/Graphonomy/eval_pascal.sh
+++
@@ -1,5 +0,0 @@
-python ./exp/test/eval_show_pascal.py \
- --batch 1 --gpus 1 --classes 7 \
- --gt_path './data/datasets/pascal/SegmentationPart/' \
- --txt_file './data/datasets/pascal/list/val_id.txt' \
- --loadmodel './cihp2pascal.pth'
--- junk.py
+++
@@ -1,3 +0,0 @@
-import numpy as np 
-x = np.load('/data/pantea/video_conf/minidataset/keypoints/train/id00622/0AqZX4K9NJc/00001/0.npy')
-print(x)
