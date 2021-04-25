7f18c4580e63cc1d0c29ca4cd99ff259e8564c41
Vedantha Venkatapathy <vedantha@thousandeyes.csail.mit.edu>
2021-04-25 15:08:38 -0400

    Added psnr

---
+++ original_bilayer/losses/PSNR.py
@@ -0,0 +1,39 @@
+# Third party
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+# This project
+from runners import utils as rn_utils
+
+
+
+class LossWrapper(nn.Module):
+    @staticmethod
+    def get_args(parser):
+        pass
+    
+    def __init__(self, args):
+        super(LossWrapper, self).__init__()
+        self.apply_to = [rn_utils.parse_str_to_list(s, sep=',') for s in rn_utils.parse_str_to_list(args.psnr_loss_apply_to, sep=';')]
+    def PSNR(self, img1, img2):
+        mse = torch.mean((img1 - img2) ** 2)
+        return 20 * torch.log10(2.0 / torch.sqrt(mse))
+            
+
+    def forward(self, data_dict, losses_dict):
+        
+        for i, (tensor_name, target_tensor_name) in enumerate(self.apply_to):
+            real_imgs = data_dict[target_tensor_name]
+            fake_imgs = data_dict[tensor_name]
+            b, t = fake_imgs.shape[:2]
+            fake_imgs = fake_imgs.view(b*t, *fake_imgs.shape[2:])
+
+            if 'HalfTensor' in fake_imgs.type():  
+                real_imgs = real_imgs.type(fake_imgs.type())
+
+            real_imgs = real_imgs.view(b*t, *real_imgs.shape[2:])
+            
+            losses_dict['G_PSNR'] = self.PSNR(fake_imgs.detach(), real_imgs.detach()) # Make these metrics and don't attach gradients to them
+        return losses_dict
+
