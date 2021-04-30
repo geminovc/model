9522a1fa081ff68619a0c93074baf1cb5187b27f
Vedantha Venkatapathy <vedantha@thousandeyes.csail.mit.edu>
2021-04-30 00:40:55 -0400

    Updated logger.py to accept metrics

--- original_bilayer/logger.py
+++ original_bilayer/logger.py
@@ -25,13 +25,15 @@ class Logger(object):
 
             if args.rank == 0:
                 if args.which_epoch != 'none' and args.init_experiment_dir == '':
-                    self.losses = pickle.load(open(self.experiment_dir / 'losses.pkl', 'rb'))
+                    self.losses = pickle.load(open(self.experiment_dir / 'losses.pkl', 'rb')) 
+                    self.losses = pickle.load(open(self.experiment_dir / 'metrics.pkl', 'rb'))
                 else:
                     self.losses = {}
+                    self.metrics = {}
                 
-                self.writer = tensorboardX.SummaryWriter('/data/pantea/tensorboard')
+                self.writer = tensorboardX.SummaryWriter('/data/pantea/tensorboard') # Set this to an argument
 
-    def output_logs(self, phase, visuals, losses, time):
+    def output_logs(self, phase, visuals, losses, metrics, time):
         if not self.no_disk_write_ops:
             # Increment iter counter
             self.num_iter[phase] += 1
@@ -51,16 +53,25 @@ class Logger(object):
                 else:
                     self.losses[key] = [value]
 
-                self.writer.add_scalar(f'{key}_{phase}', value, self.num_iter[phase])
+                self.writer.add_scalar(f'losses_{key}_{phase}', value, self.num_iter[phase])
 
+            for key, value in metrics.items():
+                if key in self.metrics:
+                    self.metrics[key].append(value)
+                else:
+                    self.metrics[key] = [value]
+
+                self.writer.add_scalar(f'metrics_{key}_{phase}', value, self.num_iter[phase])
             # Save losses
             pickle.dump(self.losses, open(self.experiment_dir / 'losses.pkl', 'wb'))
+            pickle.dump(self.metrics, open(self.experiment_dir / 'metrics.pkl', 'wb'))
 
         elif self.rank != 0:
             return
 
         # Print losses
         print(', '.join('%s: %.3f' % (key, value) for key, value in losses.items()) + ', time: %.3f' % time)
+        print(', '.join('%s: %.3f' % (key, value) for key, value in metrics.items()) + ', time: %.3f' % time)
 
     def set_num_iter(self, train_iter, test_iter):
         self.num_iter = {
