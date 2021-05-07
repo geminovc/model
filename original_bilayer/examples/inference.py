edafb4fc0405caadc8b01f861284ee687ed254f4
pkarimib <pkarimib@mit.edu>
2021-05-06 20:22:48 -0400

    Update inference.py

--- original_bilayer/examples/inference.py
+++ original_bilayer/examples/inference.py
@@ -8,37 +8,45 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import time
 from infer import InferenceWrapper
+import argparse
 
-# args_dict = {
-#     'project_dir': '/data/pantea/',
-#     'init_experiment_dir': '/data/pantea/runs/vc2-hq_adrianb_paper_main',
-#     'init_networks': 'identity_embedder, texture_generator, keypoints_embedder, inference_generator',
-#     'init_which_epoch': '2225',
-#     'num_gpus': 1,
-#     'experiment_name': 'vc2-hq_adrianb_paper_enhancer',
-#     'which_epoch': '1225',
-#     'metrics': 'PSNR, lpips',
-#     'psnr_loss_apply_to': 'pred_target_delta_lf_rgbs, target_imgs',
-#     'spn_networks': 'identity_embedder, texture_generator, keypoints_embedder, inference_generator, texture_enhancer',
-#     'enh_apply_masks': False,
-#     'inf_apply_masks': False}
 
-experiment_name = 'from_base_apply_mask_false'
-##from_base_apply_mask_false_skip_test_false
-##from_checkpoints_paper_apply_mask_false_skip_test_false
+
+
+
+# parser = argparse.ArgumentParser()
+# # General options
+# parser.add_argument('--experiment_dir',default='.', type=str, help='directory to save logs')
+# parser.add_argument('--pretrained_weights_dir',  default='/video_conf/scratch/pantea', type=str,help='directory for pretrained weights of loss networks (lpips , ...)')
+# parser.add_argument('--experiment_dir', default='.', type=str, help='root directory of the experiments')
+# # Distributed options
+# parser.add_argument('--num_gpus', default=1, type=int,help='>1 enables DDP')
+# # Initialization options
+# parser.add_argument('--init_experiment_dir',default='', type=str,help='directory of the experiment used for the initialization of the networks')
+# parser.add_argument('--init_networks', default='', type=str, help='list of networks to intialize')
+# parser.add_argument('--init_which_epoch', default='none', type=str, help='epoch to initialize from')
+# parser.add_argument('--psnr_loss_apply_to', default='pred_target_delta_lf_rgbs , target_imgs', type=str, help='psnr loss to apply') 
+
+# args = parser.parse_args()
+# print(args)
+
+experiment_name = 'all_networks_frozen_except_after_upsampling_blocks_in_texture_generator_and_inference_generator'
+experiment_dir = '/video-conf/scratch/pantea_experiments_chunky'
+init_which_epoch = '100'
+source_path = 'images/20.jpg'
+target_path = 'images/109.jpg'
+
 
 args_dict = {
-    'project_dir': '/data/pantea/video_conf/one_person_dataset/per_video_dataset/results',
-    'init_experiment_dir': '/data/pantea/video_conf/one_person_dataset/per_video_dataset/results/runs/'+experiment_name ,
-    'init_networks': 'identity_embedder, keypoints_embedder, inference_generator',
-    'init_which_epoch': '3000',
+    'experiment_dir': experiment_dir,
+    'pretrained_weights_dir': '/video-conf/scratch/pantea', 
+    'init_experiment_dir': project_dir + '/runs/' + experiment_name ,
+    'init_networks': 'identity_embedder, texture_generator, keypoints_embedder, inference_generator',
+    'init_which_epoch': init_which_epoch,
     'num_gpus': 1,
     'experiment_name': experiment_name,
-    'which_epoch': '100',
-    'metrics': 'PSNR, lpips',
     'psnr_loss_apply_to': 'pred_target_delta_lf_rgbs, target_imgs',
     'spn_networks': 'identity_embedder, texture_generator, keypoints_embedder, inference_generator',
-    'enh_apply_masks': False,
     'inf_apply_masks': False}
 
 
@@ -47,15 +55,14 @@ module = InferenceWrapper(args_dict)
 
 
 input_data_dict = {
-    'source_imgs': np.asarray(Image.open('images/109.jpg')), # H x W x 3
-    'target_imgs': np.asarray(Image.open('images/20.jpg'))[None]} # B x H x W x # 3
+    'source_imgs': np.asarray(Image.open(source_path)), # H x W x 3
+    'target_imgs': np.asarray(Image.open(target_path))[None]} # B x H x W x # 3
 
 now = time.time()
 output_data_dict = module(input_data_dict)
-#print("after!")
 now_now = time.time()
 
-#print("It took ", now_now- now)
+print("It took ", str(now_now- now), "for load and inference module.")
 
 
 
@@ -70,10 +77,4 @@ def to_image(img_tensor, seg_tensor=None):
 
 
 pred_target = to_image(output_data_dict['pred_target_imgs'][0, 0])
-pred_target.save("pred_target_imgs.jpg")
-
-# pred_target = to_image(output_data_dict['pred_enh_target_imgs'][0, 0])
-# pred_target.save("pred_enh_target_imgs.jpg") 
-
-#pred_segs = to_image(output_data_dict['pred_target_segs'][0, 0])
-#pred_segs.save("pred_target_segs.jpg")
+pred_target.save(str(experiment_name )+ "_" +str(init_which_epoch) + "_" + "pred_target_imgs.jpg")
