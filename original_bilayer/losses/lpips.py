19ee9a8b795c259f58244283e7e0c4ba24d1b3ec
Vedantha Venkatapathy <vedantha@thousandeyes.csail.mit.edu>
2021-05-09 16:41:14 -0400

    removed print

--- original_bilayer/losses/lpips.py
+++ original_bilayer/losses/lpips.py
@@ -752,7 +752,6 @@ def print_network(net):
     num_params = 0
     for param in net.parameters():
         num_params += param.numel()
-    print('Network',net)
     print('Total number of parameters: %d' % num_params)
 
 ############################################################
