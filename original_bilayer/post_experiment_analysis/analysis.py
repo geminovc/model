ffc483383cc56ebdd15a35cc6fd86f1d8b607a25
pkarimib <pkarimib@mit.edu>
2021-06-07 17:00:12 -0400

    added coments to pickle_reader

--- original_bilayer/post_experiment_analysis/analysis.py
+++ original_bilayer/post_experiment_analysis/analysis.py
@@ -8,6 +8,7 @@ import os
 import csv
 import pdb 
 
+
 # This function receives a path string and outputs the .pkl file associated with it
 def load_pickle(path_string):
     # open a file, where you stored the pickled dictionary
@@ -39,14 +40,23 @@ def min_in_window (my_dict, key, window_size):
     float_values = float_values[-window_size:]
     return min(float_values) 
 
+# Load the pickle files 
+"""
+the data_dict is in the format of [['experiment_0_name', 'experiment_0_result'], ..., ['experiment_n_name', 'experiment_n_result']]
+window is the size of the average, min, and max window over which the output for each metric in experiments is computed.  
+"""
+
 data_dict = []
 data_dict.append(['G_inf',load_pickle('/video-conf/scratch/pantea_experiments_mapmaker/runs/metrics_new_keypoints_G_inf_and_last_G_tex_unfrozen/metrics.pkl')])
 data_dict.append(['No_frozen', load_pickle('/video-conf/scratch/pantea_experiments_mapmaker/runs/metrics_new_keypoints_no_frozen/metrics.pkl')])
 window = 10
 
+
 # Write the value in the csv format
 with open('metrics.csv', 'w') as f:
-    f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%('experiment','G_PSNR_min', 'G_PSNR_mean', 'G_PSNR_max', 'G_LPIPS_min','G_LPIPS_mean','G_LPIPS_max', 'G_PME_min', 'G_PME_mean','G_PME_max'))
+    f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%('experiment_name','G_PSNR_min', 'G_PSNR_mean', 'G_PSNR_max', 'G_LPIPS_min','G_LPIPS_mean','G_LPIPS_max', 'G_PME_min', 'G_PME_mean','G_PME_max'))
+    
+    #Itrate over the experiments
     for i in range(0, len(data_dict)):
         experiment_name = data_dict[i][0]
         experiment_data = data_dict[i][1]
@@ -61,3 +71,4 @@ with open('metrics.csv', 'w') as f:
                                                   max_in_window (experiment_data, 'G_PME', window),
                                                   ))
 
+print('Saving to file Successfully done!')
