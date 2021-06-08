f11557e8d5355d6c5fd7ea4a0f6527a03c0bc53b
vibhaa <vibhaa@mit.edu>
2021-06-08 10:22:36 -0400

    analysis file uses args, moved around to reorg

--- plotting_scripts/summarize_reconstuction_exps.py
+++ plotting_scripts/summarize_reconstuction_exps.py
@@ -6,17 +6,31 @@ This script loads the saved pickle files from multiple experiments and saves the
 import pickle
 import os 
 import csv
-import pdb 
+import pdb
+import argsparse
+
+parser.ArgumentParser("Data Summarizer")
+parser.add_argument('--result-file-list',
+        type=str,
+        nargs='+',
+        help="list of result files to aggregate data into csv for")
+parser.add_argument('--experiment-name-list',
+        type=str,
+        nargs='+',
+        help="associated names for diff experiments when aggregating into csv")
+parser.add_argument('--result-file-name',
+        type=str,
+        required=True,
+        help="name of csv file to write results out to")
+args = parser.parse_args()
+window = args.window
 
 
 # This function receives a path string and outputs the .pkl file associated with it
 def load_pickle(path_string):
-    # open a file, where you stored the pickled dictionary
-    file = open(path_string, 'rb')
-    # dump information to that file
-    my_dict = pickle.load(file)
-    # close the file
-    file.close()
+    pkl_file = open(path_string, 'rb')
+    my_dict = pickle.load(pkl_file)
+    pkl_file.close()
     return my_dict
 
 # This function averages over the last window_size of input dict
@@ -40,27 +54,14 @@ def min_in_window (my_dict, key, window_size):
     float_values = float_values[-window_size:]
     return min(float_values) 
 
-# Load the pickle files 
-"""
-the data_dict is in the format of [['experiment_0_name', 'experiment_0_result'], ..., ['experiment_n_name', 'experiment_n_result']]
-window is the size of the average, min, and max window over which the output for each metric in experiments is computed.  
-"""
-
-data_dict = []
-data_dict.append(['G_inf',load_pickle('/video-conf/scratch/pantea_experiments_mapmaker/runs/metrics_new_keypoints_G_inf_and_last_G_tex_unfrozen/metrics.pkl')])
-data_dict.append(['No_frozen', load_pickle('/video-conf/scratch/pantea_experiments_mapmaker/runs/metrics_new_keypoints_no_frozen/metrics.pkl')])
-window = 10
-
-
-# Write the value in the csv format
-with open('metrics.csv', 'w') as f:
-    f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%('experiment_name','G_PSNR_min', 'G_PSNR_mean', 'G_PSNR_max', 'G_LPIPS_min','G_LPIPS_mean','G_LPIPS_max', 'G_PME_min', 'G_PME_mean','G_PME_max'))
+# Load the pickle files one at a time and write to csv
+with open(args.result_file, 'w') as f:
+    f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%('experiment_name','G_PSNR_min', 'G_PSNR_mean', 
+            'G_PSNR_max', 'G_LPIPS_min','G_LPIPS_mean','G_LPIPS_max', 'G_PME_min', 'G_PME_mean','G_PME_max'))
     
-    #Itrate over the experiments
-    for i in range(0, len(data_dict)):
-        experiment_name = data_dict[i][0]
-        experiment_data = data_dict[i][1]
-        f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%(experiment_name, min_in_window (experiment_data, 'G_PSNR', window),
+    for experiment_name, result_file in zip(args.experiment_name_list, args.result_file_list):
+        experiment_data = load_pickle(result_file)
+        f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (experiment_name, min_in_window (experiment_data, 'G_PSNR', window),
                                                   average (experiment_data, 'G_PSNR', window),
                                                   max_in_window (experiment_data, 'G_PSNR', window),
                                                   min_in_window (experiment_data, 'G_LPIPS', window),
