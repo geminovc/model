ab89894892e10bfb20f424ea823097b03bf7da7a
vibhaa <vibhaa@mit.edu>
2021-06-11 18:07:21 -0400

    using a separate style file

--- plotting_scripts/metrics_bar_plot.R
+++ plotting_scripts/metrics_bar_plot.R
@@ -9,6 +9,7 @@ library(scales)
 library(sysfonts)
 library(showtext)
 library(showtextdb)
+source("style.R")
 showtext_auto()
 
 args <- commandArgs(trailingOnly=TRUE)
@@ -22,71 +23,39 @@ data$experiment_name <- factor(data$experiment_name,
 psnr_plot <- ggplot(data, aes(x=experiment_name,y=G_PSNR_mean,fill=experiment_name)) + 
         geom_bar(stat="identity", width=0.5) + 
         geom_errorbar(aes(ymin=G_PSNR_min, ymax=G_PSNR_max), width=0.2) +
+        labs(x="Augmentation", y="PSNR (dB)") + 
 
-        labs(x="Augmentation", y="PSNR (dB)") +
-        
-        theme_minimal(base_size=15) +
-        theme(axis.text.x=element_text(size=rel(1.0)), axis.text.y=element_text(size=rel(1.0))) +
         theme(legend.text=element_text(size=rel(0.9)), legend.key.size=unit(15,"points"), legend.position="none",
               legend.box.margin=margin(-10,-10,-10,-10), legend.title=element_blank(),
               legend.margin=margin(c(0,0,0,0)))
-
 ggsave(paste(plot_filename, "_psnr.pdf", sep=""), width=5.7,height=5)
 
 lpips_plot <- ggplot(data, aes(x=experiment_name,y=G_LPIPS_mean,fill=experiment_name)) + 
         geom_bar(stat="identity", width=0.5) + 
         geom_errorbar(aes(ymin=G_LPIPS_min, ymax=G_LPIPS_max), width=0.2) +
 
-        labs(x="Augmentation", y="LPIPS") +
-        
-        theme_minimal(base_size=15) +
-        theme(axis.text.x=element_text(size=rel(1.0)), axis.text.y=element_text(size=rel(1.0))) +
-        theme(legend.text=element_text(size=rel(0.9)), legend.key.size=unit(15,"points"), legend.position="none",
-              legend.box.margin=margin(-10,-10,-10,-10), legend.title=element_blank(),
-              legend.margin=margin(c(0,0,0,0)))
-
+        labs(x="Augmentation", y="LPIPS") 
 ggsave(paste(plot_filename, "_lpips.pdf", sep=""), width=5.7,height=5)
 
 pme_plot <- ggplot(data, aes(x=experiment_name,y=G_PME_mean,fill=experiment_name)) + 
         geom_bar(stat="identity", width=0.5) + 
         geom_errorbar(aes(ymin=G_PME_min, ymax=G_PME_max), width=0.2) +
 
-        labs(x="Augmentation", y="PME") +
-        
-        theme_minimal(base_size=15) +
-        theme(axis.text.x=element_text(size=rel(1.0)), axis.text.y=element_text(size=rel(1.0))) +
-        theme(legend.text=element_text(size=rel(0.9)), legend.key.size=unit(15,"points"), legend.position="none",
-              legend.box.margin=margin(-10,-10,-10,-10), legend.title=element_blank(),
-              legend.margin=margin(c(0,0,0,0)))
-
+        labs(x="Augmentation", y="PME") 
 ggsave(paste(plot_filename, "_pme.pdf", sep=""), width=5.7,height=5)
 
 csim_plot <- ggplot(data, aes(x=experiment_name,y=G_CSIM_mean,fill=experiment_name)) + 
         geom_bar(stat="identity", width=0.5) + 
         geom_errorbar(aes(ymin=G_CSIM_min, ymax=G_CSIM_max), width=0.2) +
 
-        labs(x="Augmentation", y="CSIM") +
-        
-        theme_minimal(base_size=15) +
-        theme(axis.text.x=element_text(size=rel(1.0)), axis.text.y=element_text(size=rel(1.0))) +
-        theme(legend.text=element_text(size=rel(0.9)), legend.key.size=unit(15,"points"), legend.position="none",
-              legend.box.margin=margin(-10,-10,-10,-10), legend.title=element_blank(),
-              legend.margin=margin(c(0,0,0,0)))
-
+        labs(x="Augmentation", y="CSIM") 
 ggsave(paste(plot_filename, "_csim.pdf", sep=""), width=5.7,height=5)
 
 ssim_plot <- ggplot(data, aes(x=experiment_name,y=G_SSIM_mean,fill=experiment_name)) + 
         geom_bar(stat="identity", width=0.5) + 
         geom_errorbar(aes(ymin=G_SSIM_min, ymax=G_SSIM_max), width=0.2) +
 
-        labs(x="Augmentation", y="SSIM") +
-        
-        theme_minimal(base_size=15) +
-        theme(axis.text.x=element_text(size=rel(1.0)), axis.text.y=element_text(size=rel(1.0))) +
-        theme(legend.text=element_text(size=rel(0.9)), legend.key.size=unit(15,"points"), legend.position="none",
-              legend.box.margin=margin(-10,-10,-10,-10), legend.title=element_blank(),
-              legend.margin=margin(c(0,0,0,0)))
-
+        labs(x="Augmentation", y="SSIM") 
 ggsave(paste(plot_filename, "_ssim.pdf", sep=""), width=5.7,height=5)
 
 legend <- get_legend(psnr_plot + theme(legend.position="top"))
