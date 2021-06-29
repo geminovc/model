3e083065409a0bada2ef5bfde5a03aa27dcdf9eb
pkarimib <pkarimib@mit.edu>
2021-06-29 12:37:52 -0400

    mask_source_target -> mask_source_and_target

--- original_bilayer/datasets/voxceleb2.py
+++ original_bilayer/datasets/voxceleb2.py
@@ -63,7 +63,7 @@ class DatasetWrapper(data.Dataset):
         parser.add('--augment_with_general',     default='False', type=rn_utils.str2bool, choices=[True, False],
                                                  help='gradually increase the weight of general dataset while training the per_person dataset')
 
-        parser.add('--mask_source_target',       default='True', type=rn_utils.str2bool, choices=[True, False],
+        parser.add('--mask_source_and_target',   default='True', type=rn_utils.str2bool, choices=[True, False],
                                                  help='mask the souce and target from the beginning')
 
                                                       
@@ -318,7 +318,7 @@ class DatasetWrapper(data.Dataset):
             data_dict['target_segs'] = segs[self.args.num_source_frames:]
 
 
-        if self.args.mask_source_target and self.args.output_segmentation:
+        if self.args.mask_source_and_target and self.args.output_segmentation:
             data_dict['source_imgs'] = data_dict['source_imgs'] * data_dict['source_segs'] + (-1) * (1 - data_dict['source_segs'])
             data_dict['target_imgs'] = data_dict['target_imgs'] * data_dict['target_segs'] + (-1) * (1 - data_dict['target_segs'])
         
