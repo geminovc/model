1864f2c8fb6a1b64ae516c65880ab24ff25c345c
pkarimib <pkarimib@mit.edu>
2021-06-29 15:27:01 -0400

    Update experiments.sh

--- original_bilayer/scripts/automated/experiments.sh
+++ original_bilayer/scripts/automated/experiments.sh
@@ -7,16 +7,16 @@
 # num_epochs=${6}
 # test_freq=${7}
 # metrics_freq=${8}
-# mask_source_target=${9}
+
 
 # From base experiment
-CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "no_augmentation_no_mask"   "from_base" "per_person" 2 7000 100 100 False
-CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "no_augmentation_with_mask" "from_base" "per_person" 2 7000 100 100 True
+CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "experiment"  "from_base" "per_person" 2 7000 100 100 
 
 # From paper's released checkpoint experiment
-CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "no_augmentation_no_mask"   "from_paper" "per_person" 2 7000 100 100 False
-CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "no_augmentation_with_mask" "from_base"  "per_person" 2 7000 100 100 True
-
+CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "experiment"  "from_paper" "per_person" 2 7000 100 100 
 
 # Debugging experiment
-CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "debug" "from_paper" "per_person" 2 10 1 1 True
+CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "debug"       "from_paper" "per_person" 2 10 1 1 
+
+# Per_person train/test/unseen_test 
+CUDA_VISIBLE_DEVICES=2 ./train_script.sh  "chunky" "unseen_test" "from_paper" "per_person" 2 7000 100 100 
