5b59b4de39393e04adff919e150d79dba6067726
pkarimib <pkarimib@mit.edu>
2021-07-08 19:53:57 -0400

    flag renamed

--- plotting_scripts/summarize_reconstuction_exps.py
+++ plotting_scripts/summarize_reconstuction_exps.py
@@ -1,16 +1,16 @@
 """
 This script loads the saved pickle files from multiple experiments and saves them in csv format.
 
-If you want to use metrics_bar_plot.R do not use the summarize-pose-distribution-data flag. 
-Use summarize-pose-distribution-data if you want to use metrics_group_barchart.R to form multiple-bar plots. 
+If you want to use metrics_bar_plot.R use the skip-pose-distribution-data flag. 
+Do not use skip-pose-distribution-data if you want to use metrics_group_barchart.R to form multiple-bar plots. 
 
 Sample usage for metrics_group_barchart.R:
 python summarize_reconstuction_exps.py --result-file-list pkl_path_1 pkl_path_2 
---experiment-name-list scheme_1 scheme_2 --summarize-pose-distribution-data --pose-name-list combo easy hard --result-file-name metrics.csv
+--experiment-name-list scheme_1 scheme_2  --pose-name-list combo easy hard --result-file-name metrics.csv
 
 Sample usage for metrics_bar_plot:
 python summarize_reconstuction_exps.py --result-file-list pkl_path_1 pkl_path_2 
---experiment-name-list scheme_1 scheme_2 --result-file-name metrics.csv
+--experiment-name-list scheme_1 scheme_2 --skip-pose-distribution-data --result-file-name metrics.csv
 
 """
 
@@ -31,9 +31,9 @@ parser.add_argument('--experiment-name-list',
         type=str,
         nargs='+',
         help='associated names for diff experiments when aggregating into csv')
-parser.add_argument('--summarize-pose-distribution-data',
+parser.add_argument('--skip-pose-distribution-data',
         action='store_true',
-        help='If set, the cvs generates a new column containg pose information')
+        help='If set, the cvs does not generate the column containg pose information')
 parser.add_argument('--pose-name-list',
         type=str,
         nargs='+',
@@ -81,8 +81,7 @@ def min_in_window (my_dict, key, window_size):
 
 # Load the pickle files one at a time and write to csv
 with open(args.result_file_name, 'w') as f:
-    print(args.summarize_pose_distribution_data)
-    if not args.summarize_pose_distribution_data:
+    if args.skip_pose_distribution_data:
         f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%('experiment_name','G_PSNR_min', 'G_PSNR_mean', 
                 'G_PSNR_max', 'G_LPIPS_min','G_LPIPS_mean','G_LPIPS_max', 'G_PME_min', 'G_PME_mean','G_PME_max',
                 'G_CSIM_min','G_CSIM_mean','G_CSIM_max','G_SSIM_min','G_SSIM_mean','G_SSIM_max'))
