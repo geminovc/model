d43b4c71277026b1e3ca870cdff851a8494b7b30
pkarimib <pkarimib@mit.edu>
2021-08-03 17:22:45 -0400

    removed comments

--- plotting_scripts/make_presentation_results.sh
+++ plotting_scripts/make_presentation_results.sh
@@ -1,22 +1,16 @@
 name1=original
 
-# experiment_path_1=/data/pantea/pantea_experiments_chunky/per_person/from_paper/runs/more_yaws_original_easy_diff_combo
 experiment_path_1=/data/pantea/pantea_experiments_chunky/per_person/from_paper/runs/old/close_source_target_original_easy_diff_combo
 
 name2=G_tex_frozen
 
-# experiment_path_2=/video-conf/scratch/pantea_experiments_mapmaker/per_person/from_paper/runs/more_yaws_uniform_easy_diff_combo
 experiment_path_2=/data/pantea/pantea_experiments_chunky/per_person/from_paper/runs/original_frozen_Gtex_from_identical
 
 name3=baseline
 experiment_path_3=/video-conf/scratch/pantea_experiments_mapmaker/per_person/from_paper/runs/baseline_voxceleb2_easy_diff_combo
-# experiment_path_3=/video-conf/scratch/pantea_experiments_mapmaker/per_person/from_paper/runs/more_yaws_baseline_voxceleb2_easy_diff_combo
 
 
 result_name=G_tex_frozen
-# result_name=less_yaws_orig_vs_uni_vs_baseline
-# result_name=more_yaws_orig_vs_uni_vs_baseline
-# result_name=last_week_difficult_easy_combo
 
 
 for phase in test unseen_test
@@ -34,12 +28,6 @@ combo_path_3=${experiment_path_3}/metrics_${phase}_combined_pose.pkl
 easy_path_3=${experiment_path_3}/metrics_${phase}_easy_pose.pkl
 hard_path_3=${experiment_path_3}/metrics_${phase}_hard_pose.pkl
 
-# if [[ "$phase" == "unseen_test" ]]
-# then
-#     window=1
-# else 
-#     window=1
-# fi 
 
 window=3
 
