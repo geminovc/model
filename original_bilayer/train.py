fad750ef62cbafaa1e011b6d51257783d6deb273
pkarimib <81584521+pkarimib@users.noreply.github.com>
2021-10-12 23:04:04 -0400

    yaml TODO

--- original_bilayer/train.py
+++ original_bilayer/train.py
@@ -352,7 +352,7 @@ class TrainingWrapper(object):
                         args_file.write('%s: %s\n' % (str(k), str(v)))
 
                 config_file = self.experiment_dir / 'config.yaml'
-                config_file.write_text(yaml.dump(args), "utf-8") #TODO: add the unknown args of the parser -> infer_utils
+                config_file.write_text(yaml.dump(args), "utf-8")
 
 
         # Initialize model
