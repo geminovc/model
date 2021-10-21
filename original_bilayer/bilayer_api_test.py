6212b90fd2c16811e0d4c33ee6ee082cd4a20fa2
pkarimib <pkarimib@mit.edu>
2021-10-21 10:46:39 -0400

    pr clean up

--- original_bilayer/bilayer_api_test.py
+++ original_bilayer/bilayer_api_test.py
@@ -13,24 +13,13 @@ img_base_path = '/video-conf/scratch/pantea/per_person_1_three_datasets\
 source_img_path = img_base_path + '0.jpg'
 target_img_path = img_base_path + '10.jpg'
 
+# Bilayer Model Prediction
 source_frame = np.asarray(Image.open(source_img_path))
 target_frame = np.asarray(Image.open(target_img_path))
-
 source_poses = model.extract_keypoints(source_frame)
 model.update_source(source_frame, source_poses)
-
 target_poses = model.extract_keypoints(target_frame)
-
-# Predict
 predicted_target = model.predict(target_poses)
+
 if if_save:
     predicted_target.save("10_pred_target.png")
-
-target_img_path = img_base_path + '100.jpg'
-target_frame = np.asarray(Image.open(target_img_path))
-target_poses = model.extract_keypoints(target_frame)
-
-# Predict
-predicted_target = model.predict(target_poses)
-if if_save:
-    predicted_target.save("100_pred_target.png")
