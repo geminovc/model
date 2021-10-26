305625e82d68dc0043128aad8ff8662236adf16f
vibhaa <vibhaa@mit.edu>
2021-10-26 15:52:43 -0400

    adding experiment name

--- first_order_model/run.py
+++ first_order_model/run.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     parser.add_argument("--config", required=True, help="path to config")
     parser.add_argument("--mode", default="train", choices=["train", "reconstruction", "animate"])
     parser.add_argument("--log_dir", default='log', help="path to log into")
+    parser.add_argument("--experiment_name", default='vox-256-standard', help="experiment name to save logs")
     parser.add_argument("--checkpoint", default=None, help="path to checkpoint to restore")
     parser.add_argument("--device_ids", default="0", type=lambda x: list(map(int, x.split(','))),
                         help="Names of the devices comma separated.")
@@ -40,11 +41,8 @@ if __name__ == "__main__":
     with open(opt.config) as f:
         config = yaml.load(f)
 
-    if opt.checkpoint is not None:
-        log_dir = os.path.join(*os.path.split(opt.checkpoint)[:-1])
-    else:
-        log_dir = os.path.join(opt.log_dir, os.path.basename(opt.config).split('.')[0])
-        log_dir += ' ' + strftime("%d_%m_%y_%H.%M.%S", gmtime())
+    log_dir = os.path.join(opt.log_dir, opt.experiment_name)
+    log_dir += ' ' + strftime("%d_%m_%y_%H.%M.%S", gmtime())
 
     generator = OcclusionAwareGenerator(**config['model_params']['generator_params'],
                                         **config['model_params']['common_params'])
