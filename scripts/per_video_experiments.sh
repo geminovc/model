535377ebde70e6d88003aafa53d4f0c8d7d3f0ab
pkarimib <pkarimib@mit.edu>
2021-10-27 12:09:32 -0400

    style

--- scripts/per_video_experiments.sh
+++ scripts/per_video_experiments.sh
@@ -1,9 +1,14 @@
 # This script runs the per_video experiments
-# (fine-uning the Bilayer on a single video) with also trying freezing different parts of the Bilayer network
-# After training, the script uses inference piepline to generate predicted unseen image and video session strips accordingly
-
+# (fine-uning the Bilayer on a single video) with also trying freezing
+# different parts of the Bilayer network
+# After training, the script uses inference piepline to generate predicted
+# unseen image and video session strips accordingly
 
 run_per_video_experiments () {
+    # arg1: name of the frozen networks
+    # arg2: unfreezes the texture generator's last layers (after AdaSpade layer) if True
+    # arg3: unfreezes the inference generator's last layers (after AdaSpade layer) if True
+    # arg4: name of the experiment
     frozen_networks="$1"
     unfreeze_texture_generator_last_layers="$2"
     unfreeze_inference_generator_last_layers="$3"
@@ -11,7 +16,6 @@ run_per_video_experiments () {
     
     for video_id in  V4nIKszy_gc V9mbKUqFx0o  M_u0SV9wLro W4RJtdXRz-c fd_mtL88o1k
     do 
-
         if  [ "$do_training" = true ]
         then
             cd ${nets_repo}/original_bilayer/generate_dataset
@@ -31,12 +35,11 @@ run_per_video_experiments () {
             ${root_to_yaws} "${frozen_networks}" "${unfreeze_texture_generator_last_layers}" \
             "${unfreeze_inference_generator_last_layers}" ${experiment_dir} ${wpr_loss_weight} \
             "${replace_Gtex_output_with_source}"
-
         fi
+
         cd ${nets_repo}/scripts
 
         video_path=$(ls /video-conf/vedantha/voxceleb2/dev/mp4/id00015/${video_id} | sort -V | tail -n 1)
-
         generate_bilayer_results=true
         generate_per_person_results=true
         difficult_pose_flag=true
@@ -53,12 +56,12 @@ run_per_video_experiments () {
         per_video_flag=true
         save_dir=${experiment_logs}/${experiments_name}/videos/${video_id}
         relative_path_base=unseen_test/id00015/${video_id}/${video_path%.*}
+
         ./make_videos.sh $relative_path_base $save_dir $video_id $generate_bilayer_results \
         $generate_per_person_results $difficult_pose_flag $per_video_flag $nets_repo $experiment_dir \
         $experiments_name $num_epochs
 
     done
- 
 }
 
 data_root=/data/pantea/datasets/per_video_freezing_dataset
@@ -71,18 +74,14 @@ nets_repo=/home/pantea/NETS/nets_implementation
 experiment_logs=/data/pantea/Experiments/per_video_freezing/per_video_freezing_logs
 experiment_name_prefix=
 do_training=false
-#if with_frozen_last_layer_flag is true, experiments in which the last layer of G_inf or G_tex is frozen are conducted
+# if with_frozen_last_layer_flag is true, experiments in which the last layer 
+# of G_inf or G_tex is frozen are conducted
 with_frozen_last_layer_flag=false
-#if single_last_layer_flag is set to true, experiments in which only the last layer of G_inf or G_tex is unfrozen are conducted
+# if single_last_layer_flag is true, experiments in which only the last
+# layer of G_inf or G_tex is unfrozen are conducted
 single_last_layer_flag=false
 
-
-# The following experiments try freezing different parts of the Bilayer networks and train on a single video
-# The frist argument is the name of the networks that are frozen
-# The second argument is a boolan and unfreezes the texture generator's last layers (after AdaSpade layer) if True
-# The third argument is a boolan and unfreezes the inference generator's last layers (after AdaSpade layer) if True
-# The fourth argument is the name of the experiment
-
+# Run per_video experiments
 run_per_video_experiments ' ' 'True' 'True' ${experiment_name_prefix}no_frozen
 
 run_per_video_experiments "identity_embedder, keypoints_embedder" \
@@ -105,7 +104,6 @@ ${experiment_name_prefix}unfrozen_G_inf_and_last_layer_of_G_tex
 
 if  [ "$with_frozen_last_layer_flag" = true ] 
 then 
-
     run_per_video_experiments 'identity_embedder, keypoints_embedder' 'False' 'True' \
     ${experiment_name_prefix}unfrozen_G_tex_with_frozen_last_layer_and_G_inf
 
@@ -126,7 +124,6 @@ then
     
     run_per_video_experiments "$frozen_networks_without_Ginf" 'False' 'True' \
     ${experiment_name_prefix}unfrozen_G_inf_with_frozen_last_layer_and_last_layer_of_G_tex
-
 fi
 
 if  [ "$single_last_layer_flag" = true ] 
@@ -140,5 +137,4 @@ then
     
     run_per_video_experiments "$all_networks" 'True' 'True' \
     ${experiment_name_prefix}unfrozen_last_layer_G_tex_and_last_layer_G_inf
-
 fi
