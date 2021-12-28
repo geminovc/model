4bd07892ff6f7815a30e834e24c3cb9f805498e3
pkarimib <pkarimib@mit.edu>
2021-12-28 11:47:37 -0500

    added deepsparse

---
+++ first_order_model/onnx/deepsparse_run.py
@@ -0,0 +1,17 @@
+from deepsparse import compile_model
+from deepsparse.utils import generate_random_inputs
+import time
+
+onnx_filepath = "fom.onnx"
+batch_size = 1
+
+# Generate random sample input
+inputs = generate_random_inputs(onnx_filepath, batch_size)
+
+# Compile and run
+engine = compile_model(onnx_filepath, batch_size)
+
+for i in range(0, 100):
+	ss = time.time()
+	outputs = engine.run(inputs)
+	print("Deesparse inference time", time.time() - ss)                                                      
