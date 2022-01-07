d9f53fc31ead9da50fb1b76e56decc57d7e9ac09
pkarimib <pkarimib@mit.edu>
2022-01-06 20:14:16 -0500

    add parser

--- first_order_model/onnx/deepsparse_run.py
+++ first_order_model/onnx/deepsparse_run.py
@@ -1,17 +1,29 @@
+print("Please this code in a Linux system. Deepsparse does not support MacOS.")
+
 from deepsparse import compile_model
 from deepsparse.utils import generate_random_inputs
 import time
+import argparse
 
-onnx_filepath = "fom_gen.onnx"
-batch_size = 1
+parser = argparse.ArgumentParser(description='Convert model to deepsparse')
+parser.add_argument('--onnx_path',
+                        type = str,
+                        default = "fom_gen.onnx",
+                        help = 'path to the onnx file')
 
-# Generate random sample input
-inputs = generate_random_inputs(onnx_filepath, batch_size)
+if __name__ == '__main__':
+    args = parser.parse_args()
+    onnx_filepath = args.onnx_path
+    batch_size = 1
 
-# Compile and run
-engine = compile_model(onnx_filepath, batch_size)
+    # Generate random sample input
+    inputs = generate_random_inputs(onnx_filepath, batch_size)
 
-for i in range(0, 100):
-	ss = time.time()
-	outputs = engine.run(inputs)
-	print("Deesparse inference time", time.time() - ss)                                                      
+    # Compile and run
+    engine = compile_model(onnx_filepath, batch_size)
+
+    for i in range(0, 100):
+        ss = time.time()
+        outputs = engine.run(inputs)
+        print("Deesparse inference time", time.time() - ss)
+
