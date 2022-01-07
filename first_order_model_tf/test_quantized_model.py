2e2e1e4e5e549c7d885dee42d59ef511027c73bd
pkarimib <pkarimib@mit.edu>
2022-01-07 10:20:51 -0500

    style

--- first_order_model_tf/test_quantized_model.py
+++ first_order_model_tf/test_quantized_model.py
@@ -2,19 +2,13 @@ import numpy as np
 import tensorflow as tf
 import time
 
-## Using input/output names
-
 # Load the TFLite model and allocate tensors.
 generator_interpreter = tf.lite.Interpreter(model_path="tflite/vox_float16/generator.tflite")
 generator_interpreter.allocate_tensors()
-
-# Get input and output tensors.
 input_details = generator_interpreter.get_input_details()
 output_details = generator_interpreter.get_output_details()
 
-
 for i in range(0, 100):
-    # Initialize the inputs
     for j in range(0, len(input_details)):
         # Test the model on random input data.
         input_shape = input_details[j]['shape']
