f5d5630ff156e49c0f0c293e1645bb79562decf7
vibhaa <vibhaa@mit.edu>
2022-03-26 16:06:28 +0000

    moving all data as uint8 instead of float32

--- first_order_model/fom_wrapper.py
+++ first_order_model/fom_wrapper.py
@@ -70,17 +70,28 @@ class FirstOrderModel(KeypointBasedFaceModels):
         self.start = torch.cuda.Event(enable_timing=timing_enabled)
         self.end = torch.cuda.Event(enable_timing=timing_enabled)
 
+
     def reset(self):
+        """ reset stats """
         self.times = []
+
 
     def update_source(self, index, source_frame, source_keypoints):
         """ update the source and keypoints the frame is using 
             from the RGB source provided as input
         """
-        transformed_source = np.array([img_as_float32(source_frame)])
-        transformed_source = transformed_source.transpose((0, 3, 1, 2))
-        self.source_frames[index] = torch.from_numpy(transformed_source)
-        self.source_keypoints[index] = source_keypoints
+        transformed_source = source_frame.transpose((2, 0, 1))
+        transformed_source = torch.from_numpy(transformed_source).to(torch.uint8)
+        if torch.cuda.is_available():
+            transformed_source = transformed_source.cuda()
+        
+        # convert to float
+        transformed_source = transformed_source.to(torch.float32)
+        transformed_source = torch.div(transformed_source, 255)
+        transformed_source = torch.unsqueeze(transformed_source, 0)
+        self.source_frames[index] = transformed_source
+        
+        self.source_keypoints[index] = self.convert_kp_dict_to_tensors(source_keypoints)
 
 
     def extract_keypoints(self, frame):
@@ -88,16 +99,19 @@ class FirstOrderModel(KeypointBasedFaceModels):
             from the provided RGB image
             uses keypoints to detect the best source image to use
         """
-        transformed_frame = np.array([img_as_float32(frame)])
-        transformed_frame = transformed_frame.transpose((0, 3, 1, 2))
-
-        frame = torch.from_numpy(transformed_frame)
+        transformed_frame = frame.transpose((2, 0, 1))
+        transformed_frame = torch.from_numpy(transformed_frame).to(torch.uint8)
         if torch.cuda.is_available():
-            frame = frame.cuda() 
-        keypoint_struct = self.kp_detector(frame)
-
+            transformed_frame = transformed_frame.cuda()
+        
+        # convert to float
+        transformed_frame = transformed_frame.to(torch.float32)
+        transformed_frame = torch.div(transformed_frame, 255)
+        transformed_frame = torch.unsqueeze(transformed_frame, 0)
+        
         # change to arrays and standardize
         # Note: keypoints are stored at key 'value' in FOM
+        keypoint_struct = self.kp_detector(transformed_frame)
         keypoint_struct['value'] = keypoint_struct['value'].data.cpu().numpy()[0]
         keypoint_struct['keypoints'] = keypoint_struct.pop('value')
         if 'jacobian' in keypoint_struct:
@@ -143,10 +157,7 @@ class FirstOrderModel(KeypointBasedFaceModels):
         assert(source_index in self.source_keypoints)
         assert(source_index in self.source_frames) 
 
-        if torch.cuda.is_available():
-            self.source_frames[source_index] = self.source_frames[source_index].cuda()
-
-        source_kp_tensors = self.convert_kp_dict_to_tensors(self.source_keypoints[source_index])
+        source_kp_tensors = self.source_keypoints[source_index]
         target_kp_tensors = self.convert_kp_dict_to_tensors(target_keypoints)
 
         self.start.record()
@@ -158,7 +169,7 @@ class FirstOrderModel(KeypointBasedFaceModels):
         self.times.append(last_val)
         print(np.mean(self.times), len(self.times), last_val)
 
-        prediction_cpu = out['prediction'].data.cpu().numpy()
-        prediction = np.transpose(prediction_cpu, [0, 2, 3, 1])[0]
-        return (255 * prediction).astype(np.uint8)
-
+        prediction = torch.mul(out['prediction'][0], 255).to(torch.uint8)
+        prediction_cpu = prediction.data.cpu().numpy()
+        final_prediction = np.transpose(prediction_cpu, [1, 2, 0])
+        return final_prediction
