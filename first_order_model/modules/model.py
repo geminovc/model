132f10963377aa92861e13a0b6ac855f9e712d1f
vibhaa <vibhaa@mit.edu>
2022-03-28 14:00:39 -0400

    another temporary timing fix

--- first_order_model/modules/model.py
+++ first_order_model/modules/model.py
@@ -158,7 +158,7 @@ class GeneratorFullModel(torch.nn.Module):
         kp_source = self.kp_extractor(x['source'])
         kp_driving = self.kp_extractor(x['driving'])
 
-        generated = self.generator(x['source'], kp_source=kp_source, kp_driving=kp_driving)
+        generated, _ = self.generator(x['source'], kp_source=kp_source, kp_driving=kp_driving)
         generated.update({'kp_source': kp_source, 'kp_driving': kp_driving})
 
         loss_values = {}
