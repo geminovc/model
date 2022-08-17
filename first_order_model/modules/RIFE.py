5ffbb1643f562b9c9424f7c3f947f1f9101e7e6f
vibhaa <vibhaa@mit.edu>
2022-08-17 14:15:20 -0400

    cleaned up some unnecesary parts of RIFE

--- first_order_model/modules/RIFE.py
+++ first_order_model/modules/RIFE.py
@@ -3,30 +3,23 @@ import torch.nn as nn
 import numpy as np
 from torch.optim import AdamW
 import torch.optim as optim
+import torch.nn.functional as F
 import itertools
 from model.warplayer import warp
 from torch.nn.parallel import DistributedDataParallel as DDP
-from model.IFNet import *
-from model.IFNet_m import *
-import torch.nn.functional as F
-from model.refine import *
+from first_order_model.modules.IFNet_m import *
+from first_order_model.modules.refine import *
 
 device = torch.device("cuda")
     
 class Model:
     def __init__(self, local_rank=-1, arbitrary=False):
-        if arbitrary == True:
-            self.flownet = IFNet_m()
-        else:
-            self.flownet = IFNet()
+        self.flownet = IFNet_m()
         self.device()
-        self.optimG = AdamW(self.flownet.parameters(), lr=1e-6, weight_decay=1e-3) # use large weight decay may avoid NaN loss
-        self.epe = EPE()
-        self.lap = LapLoss()
-        self.sobel = SOBEL()
-        if local_rank != -1:
-            self.flownet = DDP(self.flownet, device_ids=[local_rank], output_device=local_rank)
-
+        
+        # use large weight decay may avoid NaN loss
+        self.optimG = AdamW(self.flownet.parameters(), lr=1e-6, weight_decay=1e-3)
+        
     def train(self):
         self.flownet.train()
 
@@ -55,11 +48,13 @@ class Model:
         for i in range(3):
             scale_list[i] = scale_list[i] * 1.0 / scale
         imgs = torch.cat((img0, img1), 1)
-        flow, mask, merged, flow_teacher, merged_teacher, loss_distill = self.flownet(imgs, scale_list, timestep=timestep)
+        flow, mask, merged, flow_teacher, merged_teacher, loss_distill = \
+                self.flownet(imgs, scale_list, timestep=timestep)
         if TTA == False:
             return merged[2]
         else:
-            flow2, mask2, merged2, flow_teacher2, merged_teacher2, loss_distill2 = self.flownet(imgs.flip(2).flip(3), scale_list, timestep=timestep)
+            flow2, mask2, merged2, flow_teacher2, merged_teacher2, loss_distill2 = \
+                    self.flownet(imgs.flip(2).flip(3), scale_list, timestep=timestep)
             return (merged[2] + merged2[2].flip(2).flip(3)) / 2
     
     def update(self, imgs, gt, learning_rate=0, mul=1, training=True, flow_gt=None):
@@ -71,12 +66,12 @@ class Model:
             self.train()
         else:
             self.eval()
-        flow, mask, merged, flow_teacher, merged_teacher, loss_distill = self.flownet(torch.cat((imgs, gt), 1), scale=[4, 2, 1])
-        loss_l1 = (self.lap(merged[2], gt)).mean()
-        loss_tea = (self.lap(merged_teacher, gt)).mean()
+        flow, mask, merged, flow_teacher, merged_teacher, loss_distill = \
+                self.flownet(torch.cat((imgs, gt), 1), scale=[4, 2, 1])
+        
         if training:
             self.optimG.zero_grad()
-            loss_G = loss_l1 + loss_tea + loss_distill * 0.01 # when training RIFEm, the weight of loss_distill should be 0.005 or 0.002
+            loss_G = 0 # TODO: what's the right loss?
             loss_G.backward()
             self.optimG.step()
         else:
