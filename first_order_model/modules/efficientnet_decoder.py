d09b51bbc711114fbfcac9547d967d227048057c
vibhaa <vibhaa@mit.edu>
2023-02-13 19:40:02 -0500

    cleaning up some overlapping code with encoder

--- first_order_model/modules/efficientnet_decoder.py
+++ first_order_model/modules/efficientnet_decoder.py
@@ -22,141 +22,14 @@ from first_order_model.modules.efficientnet_utils import (
     calculate_output_image_size
 )
 
-
-VALID_MODELS = (
-    'efficientnet-b0', 'efficientnet-b1', 'efficientnet-b2', 'efficientnet-b3',
-    'efficientnet-b4', 'efficientnet-b5', 'efficientnet-b6', 'efficientnet-b7',
-    'efficientnet-b8',
-
-    # Support the construction of 'efficientnet-l2' without pretrained weights
-    'efficientnet-l2'
+from first_order_model.modules.efficientnet_encoder import (
+    MBConvBlock,
+    VALID_MODELS
 )
 
 
-class MBConvBlock(nn.Module):
-    """Mobile Inverted Residual Bottleneck Block.
-
-    Args:
-        block_args (namedtuple): BlockArgs, defined in utils.py.
-        global_params (namedtuple): GlobalParam, defined in utils.py.
-        image_size (tuple or list): [image_height, image_width].
-
-    References:
-        [1] https://arxiv.org/abs/1704.04861 (MobileNet v1)
-        [2] https://arxiv.org/abs/1801.04381 (MobileNet v2)
-        [3] https://arxiv.org/abs/1905.02244 (MobileNet v3)
-    """
-
-    def __init__(self, block_args, global_params, image_size=None):
-        super().__init__()
-        self._block_args = block_args
-        self._bn_mom = 1 - global_params.batch_norm_momentum  # pytorch's difference from tensorflow
-        self._bn_eps = global_params.batch_norm_epsilon
-        self.has_se = (self._block_args.se_ratio is not None) and (0 < self._block_args.se_ratio <= 1)
-        self.id_skip = block_args.id_skip  # whether to use skip connection and drop connect
-
-        # Expansion phase (Inverted Bottleneck)
-        inp = self._block_args.input_filters  # number of input channels
-        oup = self._block_args.input_filters * self._block_args.expand_ratio  # number of output channels
-        if self._block_args.expand_ratio != 1:
-            Conv2d = get_same_padding_conv2d(image_size=image_size)
-            self._expand_conv = Conv2d(in_channels=inp, out_channels=oup, kernel_size=1, bias=False)
-            self._bn0 = nn.BatchNorm2d(num_features=oup, momentum=self._bn_mom, eps=self._bn_eps)
-            # image_size = calculate_output_image_size(image_size, 1) <-- this wouldn't modify image_size
-
-        # Depthwise convolution phase
-        k = self._block_args.kernel_size
-        s = self._block_args.stride
-        Conv2d = get_same_padding_conv2d(image_size=image_size)
-        self._depthwise_conv = Conv2d(
-            in_channels=oup, out_channels=oup, groups=oup,  # groups makes it depthwise
-            kernel_size=k, stride=s, bias=False)
-        self._bn1 = nn.BatchNorm2d(num_features=oup, momentum=self._bn_mom, eps=self._bn_eps)
-        image_size = calculate_output_image_size(image_size, s)
-
-        # Squeeze and Excitation layer, if desired
-        if self.has_se:
-            Conv2d = get_same_padding_conv2d(image_size=(1, 1))
-            num_squeezed_channels = max(1, int(self._block_args.input_filters * self._block_args.se_ratio))
-            self._se_reduce = Conv2d(in_channels=oup, out_channels=num_squeezed_channels, kernel_size=1)
-            self._se_expand = Conv2d(in_channels=num_squeezed_channels, out_channels=oup, kernel_size=1)
-
-        # Pointwise convolution phase
-        final_oup = self._block_args.output_filters
-        Conv2d = get_same_padding_conv2d(image_size=image_size)
-        self._project_conv = Conv2d(in_channels=oup, out_channels=final_oup, kernel_size=1, bias=False)
-        self._bn2 = nn.BatchNorm2d(num_features=final_oup, momentum=self._bn_mom, eps=self._bn_eps)
-        self._swish = MemoryEfficientSwish()
-
-    def forward(self, inputs, drop_connect_rate=None):
-        """MBConvBlock's forward function.
-
-        Args:
-            inputs (tensor): Input tensor.
-            drop_connect_rate (bool): Drop connect rate (float, between 0 and 1).
-
-        Returns:
-            Output of this block after processing.
-        """
-
-        # Expansion and Depthwise Convolution
-        x = inputs
-        if self._block_args.expand_ratio != 1:
-            x = self._expand_conv(inputs)
-            print("after expand features", x.shape)
-            x = self._bn0(x)
-            print("after bn", x.shape)
-            x = self._swish(x)
-            print("after swish", x.shape)
-
-        x = self._depthwise_conv(x)
-        print("after depthwise", x.shape)
-        x = self._bn1(x)
-        print("after bn", x.shape)
-        x = self._swish(x)
-        print("after swish", x.shape)
-
-        # Squeeze and Excitation
-        if self.has_se:
-            x_squeezed = F.adaptive_avg_pool2d(x, 1)
-            print("after adaptive pool", x_squeezed.shape)
-            x_squeezed = self._se_reduce(x_squeezed)
-            print("after se reduce", x_squeezed.shape)
-            x_squeezed = self._swish(x_squeezed)
-            print("after swish", x_squeezed.shape)
-            x_squeezed = self._se_expand(x_squeezed)
-            print("after se expand", x_squeezed.shape)
-            x = torch.sigmoid(x_squeezed) * x
-            print("after sigmoid", x.shape)
-
-        # Pointwise Convolution
-        x = self._project_conv(x)
-        print("after project conv", x.shape)
-        x = self._bn2(x)
-        print("after bn 2", x.shape)
-
-        # Skip connection and drop connect
-        input_filters, output_filters = self._block_args.input_filters, self._block_args.output_filters
-        if self.id_skip and self._block_args.stride == 1 and input_filters == output_filters:
-            # The combination of skip connection and drop connect brings about stochastic depth.
-            if drop_connect_rate:
-                x = drop_connect(x, p=drop_connect_rate, training=self.training)
-                print("after drop connect", x.shape)
-            x = x + inputs  # skip connection
-            print("after skip", x.shape)
-        return x
-
-    def set_swish(self, memory_efficient=True):
-        """Sets swish function as memory efficient (for training) or standard (for export).
-
-        Args:
-            memory_efficient (bool): Whether to use memory-efficient version of swish.
-        """
-        self._swish = MemoryEfficientSwish() if memory_efficient else Swish()
-
-
 class EfficientNetDecoder(nn.Module):
-    """EfficientNet model.
+    """EfficientNet model version of decoder, mimicked from the encoder
        Most easily loaded with the .from_name or .from_pretrained methods.
 
     Args:
@@ -182,9 +55,11 @@ class EfficientNetDecoder(nn.Module):
         self._global_params = global_params
         self._blocks_args = []
 
+        # reverse block order and input/output filters
         for block_args in reversed(blocks_args):
             input_filters = block_args.input_filters
             output_filters = block_args.output_filters
+
             # Update block input and output filters based on depth multiplier.
             block_args = block_args._replace(
                 input_filters=round_filters(output_filters, self._global_params),
@@ -204,7 +79,7 @@ class EfficientNetDecoder(nn.Module):
 
         # Reversing head and stem, so this is head
         image_size = (64, 64)
-        in_channels = 512 # output of final block
+        in_channels = 512 # output of final block of encoder
         out_channels = round_filters(self._blocks_args[2].input_filters, self._global_params)
         Conv2d = get_same_padding_conv2d(image_size=image_size)
         self._conv_head = Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
@@ -260,53 +135,6 @@ class EfficientNetDecoder(nn.Module):
         self._swish = MemoryEfficientSwish() if memory_efficient else Swish()
         for block in self._blocks:
             block.set_swish(memory_efficient)
-
-    def extract_endpoints(self, inputs):
-        """Use convolution layer to extract features
-        from reduction levels i in [1, 2, 3, 4, 5].
-
-        Args:
-            inputs (tensor): Input tensor.
-
-        Returns:
-            Dictionary of last intermediate features
-            with reduction levels i in [1, 2, 3, 4, 5].
-            Example:
-                >>> import torch
-                >>> from efficientnet.model import EfficientNet
-                >>> inputs = torch.rand(1, 3, 224, 224)
-                >>> model = EfficientNet.from_pretrained('efficientnet-b0')
-                >>> endpoints = model.extract_endpoints(inputs)
-                >>> print(endpoints['reduction_1'].shape)  # torch.Size([1, 16, 112, 112])
-                >>> print(endpoints['reduction_2'].shape)  # torch.Size([1, 24, 56, 56])
-                >>> print(endpoints['reduction_3'].shape)  # torch.Size([1, 40, 28, 28])
-                >>> print(endpoints['reduction_4'].shape)  # torch.Size([1, 112, 14, 14])
-                >>> print(endpoints['reduction_5'].shape)  # torch.Size([1, 320, 7, 7])
-                >>> print(endpoints['reduction_6'].shape)  # torch.Size([1, 1280, 7, 7])
-        """
-        endpoints = dict()
-
-        # Stem
-        x = self._swish(self._bn0(self._conv_stem(inputs)))
-        prev_x = x
-
-        # Blocks
-        for idx, block in enumerate(self._blocks):
-            drop_connect_rate = self._global_params.drop_connect_rate
-            if drop_connect_rate:
-                drop_connect_rate *= float(idx) / len(self._blocks)  # scale drop connect_rate
-            x = block(x, drop_connect_rate=drop_connect_rate)
-            if prev_x.size(2) > x.size(2):
-                endpoints['reduction_{}'.format(len(endpoints) + 1)] = prev_x
-            elif idx == len(self._blocks) - 1:
-                endpoints['reduction_{}'.format(len(endpoints) + 1)] = x
-            prev_x = x
-
-        # Head
-        x = self._swish(self._bn1(self._conv_head(x)))
-        endpoints['reduction_{}'.format(len(endpoints) + 1)] = x
-
-        return endpoints
 
     def extract_features(self, inputs):
         """use convolution layer to extract feature .
