77fa88da081a3096bd8bebb024b6931f649afeb2
vibhaa <vibhaa@mit.edu>
2023-02-15 15:39:37 -0800

    Revert "removing size prints"
    This reverts commit 347700f719cde0fe036d04fbdb7187b2c0248c6b.

--- first_order_model/modules/efficientnet_decoder.py
+++ first_order_model/modules/efficientnet_decoder.py
@@ -148,6 +148,7 @@ class EfficientNetDecoder(nn.Module):
         """
         # Head
         x = self._swish(self._bn1(self._conv_head(inputs)))
+        print("after swish 2", x.shape)
 
         # Blocks
         for idx, block in enumerate(self._blocks):
@@ -158,10 +159,14 @@ class EfficientNetDecoder(nn.Module):
                 x = block(x)
             else:
                 x = block(x, drop_connect_rate=drop_connect_rate)
+            print("after block", idx, x.shape)
 
         # Stem
+        print("in extract features", inputs.shape)
         x = self._upsample(x)
+        print("after upsample", x.shape)
         x = self._swish(self._bn0(self._conv_stem(x)))
+        print("after swish before blocks", x.shape)
 
         return x
 
@@ -176,15 +181,21 @@ class EfficientNetDecoder(nn.Module):
             Output of this model after processing.
         """
         # Convolution layers
+        print("starting extraction", inputs.shape)
         x = self.extract_features(inputs)
+        print("after features", x.shape)
         
         # Pooling and final linear layer
         """
         x = self._avg_pooling(x)
+        print("after pooling", x.shape)
         if self._global_params.include_top:
             x = x.flatten(start_dim=1)
+            print("after flattening", x.shape)
             x = self._dropout(x)
+            print("after dropout", x.shape)
             x = self._fc(x)
+            print("after fc", x.shape)
         """ 
         return x
 
