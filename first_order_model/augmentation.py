364509111f55589fc39560c179c12df2e50bcb09
Vedantha Venkatapathy <vedantha@mit.edu>
2023-04-07 03:21:57 +0000

    More config

--- first_order_model/augmentation.py
+++ first_order_model/augmentation.py
@@ -9,7 +9,8 @@ import numpy as np
 import PIL
 
 from skimage.transform import resize, rotate
-from skimage.util import pad
+#from skimage.util import pad
+from numpy import pad
 import torchvision
 
 import warnings
